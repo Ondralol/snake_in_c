a9a925fa6d252e076ac9ef9c44b2cb826e601e36
Ondřej Duba <dubaond3@fit.cvut.cz>
2024-08-11 15:08:10 +0200

    Added main

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define WIDTH  32
+#define HEIGHT 32
+#define SNAKECHARACTER 100 //ascii value
+
+int main (int argc, char ** argv)
+{
+  //TODO user inputs size
+  
+
+  runGame();
+
+}
