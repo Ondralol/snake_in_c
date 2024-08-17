0fa1ad23b3c1ea4e4c03665f3c745c5bbfecfaaf
Ondřej Duba <dubaond3@fit.cvut.cz>
2024-08-17 02:15:10 +0200

    gui tiles

--- test.c
+++ test.c
@@ -1,17 +1,58 @@
 #include <stdio.h>
 
-#define NORMAL "\x1B[0m"
-#define RED    "\x1B[1;31m"
-#define clear()  printf("\x1B[2J")
 
-
-#define move_cursour(X, Y) printf("\033[%d;%dH", X, Y)
-
-int main ()
+int main()
 {
-  printf("%sHello world\n%s", RED, NORMAL);
-  //clear();
-  move_cursour(1,1);
-  printf("10");
-  return 0;
+	printf("\x1B[0;32m");
+	
+	for ( int i = 0; i < 10; i ++)
+	{
+		for ( int j = 0; j < 10; j++)
+		{	
+			/* white border */
+			if ( i == 6 && j == 6 )
+      {
+			
+				printf("%s", "\x1B[0;34m");
+        printf("████ ");
+        printf("\x1B[0;32m");
+      }
+			
+			else if ( i == 3 && j == 2 )
+      {
+        printf("%s", "\x1B[0;91m");
+        printf("████ ");
+        printf("\x1B[0;32m");
+      }
+
+			else
+				printf("████ ");
+		}
+		printf("\n");
+		for ( int j = 0; j < 10; j++)
+    {
+			if ( i == 6 && j == 6 )
+			{
+				printf("%s", "\x1B[0;34m");
+				printf("████ ");
+				printf("\x1B[0;32m");
+			}
+			
+			else if ( i == 3 && j == 2 )
+      {
+        printf("%s", "\x1B[0;91m");
+        printf("████ ");
+        printf("\x1B[0;32m");
+      }
+	
+			else
+				printf("████ ");
+    }
+		printf("\n");
+		printf("\n");
+
+	}
+
+
+	return 0;
 }
